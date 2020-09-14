ed113bbf85857c34236f3206ef29a8752387a4ce
Corey Buzzell <cdbuzzell@gmail.com>
2020-09-14 11:47:39 -0500

    NotifyChannel is working for NEW and UPDATE

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# TeamsProactiveMessage
+# Teams Proactive Message
+
+I built this project because I couldn't find a simple example of how to send proactive messages to users/channels in Microsoft Teams. Every example either started with a user sending a message to my bot (and thus had pre-existing Teams context), or had a ton of other functionality wrapped around the proactive message. This project is meant to be the most basic implementation of proactive messages. This project is not production-ready nor a representation of how you should do this for reals.
+
+TODO: link to proactive messages docs
+
+## Prerequisites
+
+### Bot registration
+
+### Teams channel
+TODO: use the Teams web client to figure out the channel id
+
+TODO: install your bot in the channel (link to requirements)
+
+## How to test
+
+### Configuration
+
+### Call with PostMan
+
+TODO: include collection?
---
+++ TeamsProactiveMessageApi/TeamsProactiveMessageApi/Controllers/NotifyChannelController.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TeamsProactiveMessageApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotifyChannelController : ControllerBase
+    {
+        private static IConfiguration configuration = null;
+        private static string botAccessToken = null;
+        private static DateTime? botAccessTokenExpiration = null;
+        private static HttpClient httpClient = null;
+        //private readonly ILogger<NotifyChannelController> _logger;
+
+        //public NotifyChannelController(ILogger<NotifyChannelController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        // POST api/<NotifyChannelController>
+        [HttpPost]
+        public HttpResponseMessage Post([FromBody] string message)
+        {
+            // load configuration from appSettings
+            configuration = configuration ??
+                new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true)
+                    .AddEnvironmentVariables()
+                    .Build();
+
+            httpClient = new HttpClient();
+
+            // get bearer token to make Teams API call
+            if (botAccessToken == null
+                    || botAccessTokenExpiration == null
+                    || DateTime.UtcNow > botAccessTokenExpiration)
+            {
+                //await this.FetchTokenAsync(configuration, httpClient);
+                this.FetchTokenAsync(configuration, httpClient).Wait();
+            }
+
+            // make call to Teams REST API to POST a NEW message to a channel
+            using (var requestMessage = new HttpRequestMessage(HttpMethod.Post, configuration["ConversationsUrl"]))
+            {
+                requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", botAccessToken);
+
+                var payload = "{" +
+                    "\"bot\": {" +
+                         "\"id\": \"" + configuration["MicrosoftAppId"] + "\"" +
+                    "}," +
+                    "\"isGroup\": true," +
+                    "\"channelData\": {" +
+                        "\"channel\": {" +
+                            "\"id\": \"" + configuration["ChannelId"] + "\"" +
+                        "}," +
+                         "\"notification\": {" +
+                            "\"alert\": true" +
+                         "}" +
+                    "}," +
+                    "\"activity\": {" +
+                        "\"type\": \"message\"," +
+                        "\"text\": \"" + message + "\"," +
+                        "\"attachments\": []," +
+                        "\"entities\": []" +
+                    "}" +
+                  "}";
+
+                requestMessage.Content = new StringContent(payload, Encoding.UTF8, "application/json");
+
+                using (var sendResponse = httpClient.SendAsync(requestMessage).Result)
+                {
+                    if (sendResponse.StatusCode == HttpStatusCode.Created)
+                    {
+                        var jsonResponseString = sendResponse.Content.ReadAsStringAsync().Result;
+                        //dynamic resp = JsonConvert.DeserializeObject(jsonResponseString);
+                    }
+
+                    //return requestMessage.CreateResponse(sendResponse.StatusCode);
+
+                    //TODO: still need to return the conversation/message id
+                    HttpResponseMessage response = new HttpResponseMessage(sendResponse.StatusCode);
+                    //response.Content = sendResponse.Content;
+                    return response;
+                }
+            }
+        }
+
+        // PUT api/<NotifyChannelController>/guid
+        [HttpPut("{id}")]
+        public HttpResponseMessage Put(string id, [FromBody] string message)
+        {
+            // load configuration from appSettings
+            configuration = configuration ??
+                new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.Development.json", optional: true, reloadOnChange: true)
+                    .AddEnvironmentVariables()
+                    .Build();
+
+            httpClient = new HttpClient();
+
+            // get bearer token to make Teams API call
+            if (botAccessToken == null
+                    || botAccessTokenExpiration == null
+                    || DateTime.UtcNow > botAccessTokenExpiration)
+            {
+                this.FetchTokenAsync(configuration, httpClient).Wait();
+            }
+
+            //TODO: have them pass id AND activityid instead
+            string messageId = id.Split(";messageid=")[1];
+
+            // make call to Teams REST API to UPDATE an EXISTING message in a channel
+            var updateUrl = $"{configuration["ConversationsUrl"]}/{id}/activities/{messageId}";
+
+            using (var requestMessage = new HttpRequestMessage(HttpMethod.Put, updateUrl))
+            {
+                requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Bearer", botAccessToken);
+
+                var payloadString = "{" +
+                    "\"type\": \"message\"," +
+                    "\"text\": \"[UPDATED] :: " + message + "\"," +
+                    "\"attachments\": []," +
+                    "\"entities\": []" +
+                  "}" +
+                  "}";
+
+                requestMessage.Content = new StringContent(payloadString, Encoding.UTF8, "application/json");
+
+                using (var sendResponse = httpClient.SendAsync(requestMessage).Result)
+                {
+                    if (sendResponse.StatusCode == HttpStatusCode.OK)
+                    {
+                        var jsonResponseString = sendResponse.Content.ReadAsStringAsync().Result;
+                        //dynamic resp = JsonConvert.DeserializeObject(jsonResponseString);
+                    }
+
+                    //return requestMessage.CreateResponse(sendResponse.StatusCode);
+
+                    //TODO: still need to return the conversation/message id
+                    HttpResponseMessage response = new HttpResponseMessage(sendResponse.StatusCode);
+                    response.Content = sendResponse.Content;
+                    return response;
+                }
+
+            }
+        }
+
+        /*
+         *  UTILITY / HELPERS
+         */
+
+        private async Task FetchTokenAsync(IConfiguration configuration, HttpClient httpClient)
+        {
+            var values = new Dictionary<string, string>
+                {
+                    { "grant_type", "client_credentials" },
+                    { "client_id", configuration["MicrosoftAppId"] },
+                    { "client_secret", configuration["MicrosoftAppPassword"] },
+                    { "scope", "https://api.botframework.com/.default" },
+                };
+
+            var content = new FormUrlEncodedContent(values);
+
+            using (var tokenResponse = await httpClient.PostAsync("https://login.microsoftonline.com/botframework.com/oauth2/v2.0/token", content))
+            {
+                if (tokenResponse.StatusCode == HttpStatusCode.OK)
+                {
+                    //var accessTokenContent = await tokenResponse.Content.ReadAsAsync<AccessTokenResponse>();
+                    var accessTokenContent = JsonConvert.DeserializeObject<AccessTokenResponse>(await tokenResponse.Content.ReadAsStringAsync());
+
+                    botAccessToken = accessTokenContent.AccessToken;
+
+                    var expiresInSeconds = 121;
+
+                    // If parsing fails, out variable is set to 0, so need to set the default
+                    if (!int.TryParse(accessTokenContent.ExpiresIn, out expiresInSeconds))
+                    {
+                        expiresInSeconds = 121;
+                    }
+
+                    // Remove two minutes in order to have a buffer amount of time.
+                    botAccessTokenExpiration = DateTime.UtcNow + TimeSpan.FromSeconds(expiresInSeconds - 120);
+                }
+                else
+                {
+                    throw new Exception("Error fetching bot access token.");
+                }
+            }
+        }
+        private class AccessTokenResponse
+        {
+            [JsonProperty("token_type")]
+            public string TokenType { get; set; }
+
+            [JsonProperty("expires_in")]
+            public string ExpiresIn { get; set; }
+
+            [JsonProperty("ext_expires_in")]
+            public string ExtExpiresIn { get; set; }
+
+            [JsonProperty("access_token")]
+            public string AccessToken { get; set; }
+        }
+    }
+}
